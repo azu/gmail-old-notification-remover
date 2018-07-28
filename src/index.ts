4ab93a903d54b30c750b87fe4d28f7d0111b2dcf
azu <azuciao@gmail.com>
2018-07-28 17:42:26 +0900

    fix: filter by mail date

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,40 @@
-import GmailApp = GoogleAppsScript.Gmail.GmailApp;
-
+import dayjs = require("dayjs");
 declare var global: any;
 global.cleanupMail = cleanupMail;
+
+type SearchTargetItem = { search: string; expireBeforeDays: number };
 
 function cleanupMail() {
     /**
      * Cleanup condition
      */
-    const targets = [
+    const targets: SearchTargetItem[] = [
         {
             search: "label:notification",
-            // 365日より前のものが対象
-            expireBefore: `365d`
+            // older than 365 days
+            expireBeforeDays: 365
         }
     ];
 
-    const IgnoreRule = `-in:starred or -in:important`;
+    const IgnoreRule = `-is:starred`;
 
     // 対象ラベルのループ処理
     targets.forEach(target => {
-        const searchCondition = `${target.search} older_than:${target.expireBefore} and (${IgnoreRule})`;
+        const searchCondition = `${target.search} older_than:${target.expireBeforeDays}d ${IgnoreRule}`;
         Logger.log(`SearchCondition: ${searchCondition}`);
         const threads = GmailApp.search(searchCondition, 0, 50);
-        Logger.log(`Match threads count: ${threads.length}`);
-        if (threads.length === 0) {
+        // Gmail's thread includes newer mail than expireBeforeDays.
+        // It filter by expireBeforeDays
+        const filteredThreads = threads.filter(thread => {
+            const lastMessageDate = thread.getLastMessageDate();
+            const limitDay = dayjs().subtract(target.expireBeforeDays, "day");
+            return dayjs(lastMessageDate).isBefore(limitDay);
+        });
+        Logger.log(`Match threads count: ${filteredThreads.length}`);
+        if (filteredThreads.length === 0) {
             return;
         }
-        GmailApp.moveThreadsToTrash(threads);
-        Logger.log(`Delete ${threads.length} threads`);
+        GmailApp.moveThreadsToTrash(filteredThreads);
+        Logger.log(`Delete ${filteredThreads.length} threads`);
     });
 }
